8001f1498ce617dca688c56facb9b85f1b7e1028
JulietteDrupt <juliette.drupt@gmail.com>
2022-08-29 17:01:02 +0200

    first work on MultiAgentSystem's constructor

--- test/testMultiAgentSystem.cc
+++ test/testMultiAgentSystem.cc
@@ -8,5 +8,10 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+
+    // Constructor
+    std::string pathToVoc("../Vocabulary/ORBvoc.txt");
+    ORB_SLAM3::MultiAgentSystem mas(pathToVoc);
+
     return 0;
 }
