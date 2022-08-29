864f93c92754733abcff201684f8df2ef4c76c81
JulietteDrupt <juliette.drupt@gmail.com>
2022-08-29 14:23:50 +0200

    void test file

---
+++ test/testMultiAgentSystem.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include<opencv2/core/core.hpp>
+
+#include"../include/MultiAgentSystem.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    return 0;
+}
